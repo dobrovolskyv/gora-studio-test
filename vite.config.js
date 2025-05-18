3a64e9336dcd10480271862b143e2cedff060a58
Vlados <malak3@mail.ru>
2025-05-18 05:26:35 +0300

    FIX:deploy routes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        const src = path.resolve(__dirname, 'public/_redirects')
+        const dest = path.resolve(__dirname, 'dist/_redirects')
+        fs.copyFileSync(src, dest)
+      },
+    },
+  ],
 })
